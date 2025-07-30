95732e1855a7ea864902731b7ae53bdfd811fac9
iuriigorbaniuk <iurii.gor1980@yandex.ru>
2025-07-30 16:46:01 +0300

    Упорядочить массив [#505228]

---
+++ src/main/java/ru/job4j/array/AlgoArray.java
@@ -0,0 +1,31 @@
+package ru.job4j.array;
+
+public class AlgoArray {
+
+    public static void main(String[] args) {
+
+        // массив , меняем местами ячейки (5 и 1)
+        int[] array = new int[] {5, 3, 2, 1, 4};
+        int temp = array[0];
+        array[0] = array[3];
+        array[3] = temp;
+
+        // массив до сортировки:
+        for (int index = 0; index < array.length; index++) {
+            System.out.print(array[index] + "| ");
+        }
+
+        System.out.println();
+
+        // массив после сортировки (по возрастанию):
+        for (int index = 0; index < array.length; index++) {
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    int xTemp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = xTemp;
+                }
+            } System.out.print(array[index] + "| ");
+        }
+    }
+}
