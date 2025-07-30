05060e549a848ae57d7959c8632c96d98b739dce
iuriigorbaniuk <iurii.gor1980@yandex.ru>
2025-07-30 14:31:23 +0300

    Массивы в тестах [#505226]

---
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FindLoopTest {
+
+    @Test
+    void whenArrayHas5Then0() {
+        int[] data = new int[] {5, 10, 3};
+        int element = 5;
+        int result = FindLoop.indexOf(data, element);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenArrayHas3ThenMinus1() {
+        int[] data = new int[] {7, 8, 9};
+        int element = 3;
+        int result = FindLoop.indexOf(data, element);
+        int expected = -1;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenArrayHas6ThenMinus1() {
+        int[] data = new int[] {7, 8, 9};
+        int element = 6;
+        int result = FindLoop.indexOf(data, element);
+        int expected = -1;
+        assertThat(result).isEqualTo(expected);
+    }
+}
