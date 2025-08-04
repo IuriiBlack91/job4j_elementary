ae5508f9065383e28eb8b1216c958a6940fa4c3a
iuriigorbaniuk <iurii.gor1980@yandex.ru>
2025-08-04 20:22:56 +0300

    Перевернуть массив [#505231 #549631]

--- src/main/java/ru/job4j/array/Turn.java
+++ src/main/java/ru/job4j/array/Turn.java
@@ -3,14 +3,12 @@ package ru.job4j.array;
 public class Turn {
 
     public static int[] back(int[] array) {
-        // реализация переворота массива:
-        // i - индекс первого элемента , j - индекс последнего элемента
-        int tmp = 0;
-        for (int i = 0, j = array.length - 1; i < j; i++, j--) {
-            tmp = array[i];
-            array[i] = array[j];
-            array[j] = tmp;
+
+        int[] tempArray = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            tempArray[array.length - 1 - i] = array[i];
         }
-        return array;
+        return tempArray;
     }
 }
