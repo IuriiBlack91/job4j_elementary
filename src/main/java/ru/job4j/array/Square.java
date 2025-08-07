b165f33948aefdbb3f6328eaaac3a5431854d988
iuriigorbaniuk <iurii.gor1980@yandex.ru>
2025-08-07 09:27:26 +0300

    Массивы в тестах [#505226 #549475]

--- src/main/java/ru/job4j/array/Square.java
+++ src/main/java/ru/job4j/array/Square.java
@@ -12,6 +12,7 @@ public class Square {
         return result;
     }
 
+    //...
     public static void main(String[] args) {
         int[] array = calculate(4);
         for (int index = 0; index < array.length; index++) {
