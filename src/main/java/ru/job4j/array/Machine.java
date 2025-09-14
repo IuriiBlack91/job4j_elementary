10baa56ff0ded8c16b53dfcc4a62419ea9d14d94
iuriigorbaniuk <iurii.gor1980@yandex.ru>
2025-09-14 15:49:44 +0300

    6.8.1. Жадный алгоритм. Сдача в кофе машине [#505250 #550013]

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -3,24 +3,20 @@ package ru.job4j.array;
 import java.util.Arrays;
 
 public class Machine {
-
     public static int[] change(int money, int price) {
-
-        if (money <= price) {                       // Проверка
+        if (money <= price) {
             return new int[0];
         }
-
-        int[] coins = {10, 5, 2, 1};                // Номиналы монет, отсортированные по убыванию
-        int[] result = new int[100];                // Массив для хранения результата (максимум 100 монет)
-        int rest = money - price;                   // Сумма сдачи
-        int size = 0;                               // Начальный размер
-
-        for (int i = 0; i < coins.length; i++) {    // Проходим по всем номиналам монет
-            while (rest >= coins[i]) {              // Пока текущий номинал помещается в остаток сдачи
-                result[size++] = coins[i];          // Добавляем монету в результат
-                rest -= coins[i];                   // Уменьшаем остаток сдачи
+        int[] coins = {10, 5, 2, 1};
+        int[] result = new int[100];
+        int size = 0;
+        money -= price;
+        for (int coin : coins) {
+            while (money >= coin) {
+                result[size++] = coin;
+                money -= coin;
             }
         }
-        return Arrays.copyOf(result, size);         // Возвращаем обрезанный массив с результатом
+        return Arrays.copyOf(result, size);
     }
 }
